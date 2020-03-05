c51d5147e85fdf47b428b62626190634c1c1bb33
Jeff Klouda <jklouda1@nd.edu>
2020-03-05 13:38:15 -0500

    axis label spacing

--- venezuela_viz/viz.js
+++ venezuela_viz/viz.js
@@ -313,9 +313,9 @@ function updateData(solo) {
 
     svg.append("text")
       .attr("transform", "rotate(90)")
-      .attr("y", - width - (margin.left / 4))
+      .attr("y", - width - (margin.left / 3))
       .attr("x",0 + (height / 2))
-      .attr("dy", "1rem")
+      .attr("dy", "1em")
       .style("text-anchor", "middle")
       .style("fill", "#999")
       .text("Number of Venezuelan Migrants");
